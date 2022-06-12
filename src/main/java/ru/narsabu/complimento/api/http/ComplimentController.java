4478a85890303adc2e498a6ab88f94b7466bd7a3
Marat Usmanov <chelioschew@yandex.ru>
2022-06-12 17:40:02 +0300

    port edited + openapi

--- src/main/java/ru/narsabu/complimento/api/http/ComplimentController.java
+++ src/main/java/ru/narsabu/complimento/api/http/ComplimentController.java
@@ -1,5 +1,6 @@
 package ru.narsabu.complimento.api.http;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ public class ComplimentController {
     private final ComplimentService service;
 
     @GetMapping()
+    @Operation(summary = "get random compliment")
     public String getCompliment() {
      return service.getCompliment();
     }
